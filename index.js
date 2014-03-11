fb480ced2397468492343b99bf8a1e8397329cda
Elliot Stokes <elliot.stokes@gmail.com>
2014-03-11 20:05:57 +0000

    lint files

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-module.exports = process.env.GPXPARSE_COV
-  ? require('./lib-cov/gpx-parse')
-  : require('./lib/gpx-parse')
+module.exports = process.env.GPXPARSE_COV ? require('./lib-cov/gpx-parse') : require('./lib/gpx-parse');
