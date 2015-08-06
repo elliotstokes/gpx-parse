a6e29a49ad46ee0de2212dd39bc76ec223b8b4d5
Wilkins <thibault@taillandier.name>
2015-08-06 15:38:21 +0200

    Browserfiying the module

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,33 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		jsdoc: {
 			dist : {
-            src: ['README.md', 'lib/**/*.js'],
-            options: {
-                destination: 'doc'
+                src: ['README.md', 'lib/**/*.js'],
+                options: {
+                    destination: 'doc'
+                }
             }
+		},
+        browserify: {
+          options: {
+            browserifyOptions: {
+               standalone: 'gpx-parse'
+            }
+          },
+          dist: {
+            src: 'index.js',
+            dest: 'gpx-parse.js',
+            options: {
+              alias: [
+                // Mockup to avoid loading lib-cov/gpx-parse in the browserify build process
+                './browserify-mock.js:./lib-cov/gpx-parse'
+              ]
+            }
+          }
         }
-		}
 	});
 
 	grunt.loadNpmTasks('grunt-jsdoc');
+	grunt.loadNpmTasks('grunt-browserify');
 
 
 	grunt.registerTask('document', ['jsdoc']);
