d516e68a7182726843ad1d9f052c816cbfdcd95d
Elliot Stokes <elliot.stokes@gmail.com>
2016-02-08 19:43:37 +0000

    further refactoring of the parser

--- tests/gpx-v1.0-tests.js
+++ tests/gpx-v1.0-tests.js
@@ -94,7 +94,6 @@ module.exports = {
     },
 
 
-
     "Test that valid gpx string is parsed successfully": function(test) {
         gpxParse.parseGpx(successfulGpx, function(error, result) {
 
@@ -118,30 +117,33 @@ module.exports = {
 
     },
 
-    "Test that valid gpx file is parsed successfully": function(test) {
+    "Test that you can load in just waypoints": function(test) {
+        var wayPointGpx = ['<?xml version="1.0"?>',
+        '<gpx version="1.0" ',
+        'creator="ExpertGPS 1.1 - http://www.topografix.com" ',
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ',
+        'xmlns="http://www.topografix.com/GPX/1/0" ',
+        'xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">',
+        '<time>2002-02-27T17:18:33Z</time>',
+        '<bounds minlat="42.401051" minlon="-71.126602" maxlat="42.468655" maxlon="-71.102973"/>',
+        '<wpt lat="42.438878" lon="-71.119277">',
+        '<ele>44.586548</ele>',
+        '<time>2001-11-28T21:05:28Z</time>',
+        '<name>5066</name>',
+        '<desc><![CDATA[5066]]></desc>',
+        '<sym>Crossing</sym>',
+        '<type><![CDATA[Crossing]]></type>',
+        '</wpt>',
+        '</gpx>'].join('');
 
-        //this.fsMock.open = function(path, flags, callback) {
-        //    callback(null, successfulGpx);
-        //};
-
-        /*this.gpxParse.parseGpxFromFile("/path/to/gpxFile.gpx", function(error, result) {
-
-            test.equal(error, null);
-            test.equal(result.metadata.creator, "ExpertGPS 1.1 - http://www.topografix.com");
-            test.equal(result.metadata.time, "2002-02-27T17:18:33Z");
-            test.equal(result.waypoints.length, 2);
-            test.equal(result.waypoints[0].x, 42.438878);
-            test.equal(result.waypoints[1].y, -71.119689);
-            test.equal(result.routes.length, 1);
-            test.equal(result.routes[0].length, 4);
-            test.equal(result.routes[0][0].x, 42.43095);
-            test.equal(result.routes[0][0].y, -71.107628);
-            test.equal(result.tracks.length, 1);
-            test.equal(result.tracks[0].segments.length, 1);
-            test.equal(result.tracks[0].segments[0].length, 3);
+        gpxParse.parseGpx(wayPointGpx, function(err, result) {
+            
+            test.equal(err, null);
+            test.equal(result.tracks.length, 0);
+            test.equal(result.routes.length, 0);
+            test.equal(result.waypoints.length, 1);
+            test.equal(result.waypoints[0].name, "5066");
             test.done();
-        });*/
-        test.done();
-
+        });        
     }
 };
