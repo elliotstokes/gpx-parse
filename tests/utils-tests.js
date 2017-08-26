cac0759e12fbbae60d34f2384eb32964a173963b
Pavlo Deshko <pavlodeshko92@gmail.com>
2017-08-26 14:34:12 +0300

    calculate bearing added

--- tests/utils-tests.js
+++ tests/utils-tests.js
@@ -23,6 +23,19 @@ module.exports = {
         var midpoint = geomUtils.calculateMidpoint(4.367, 5.6745, -40.4556, 39.34345);
         test.deepEqual(midpoint, [-18.78213486, 20.18113593]);
         test.done();
+    },
+    
+    "Should calculate bearing from one point to other" : function(test){
+        var bearingWest = geomUtils.calculateBearing(0, 40, 0, 50);
+        test.equal(bearingWest, 90);
+      
+        var bearingNorth= geomUtils.calculateBearing(-50, -40, 50, -40);
+        test.equal(bearingNorth, 0);
+      
+        var bearing  = geomUtils.calculateBearing(4.367, 5.6745, -40.4556, 39.34345);
+        test.equal(bearing, 148.750703805);
+        
+        test.done();
     }
 };
 
